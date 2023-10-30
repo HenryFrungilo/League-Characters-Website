99aabfcd9fd226e7390d61b602bf7747be7ffb09
Henry Alexandre de Almeida Frungilo <henryfrungilo@gmail.com>
2023-10-29 21:02:36 -0300

    Buttons Features

--- src/app/pages/mainpage/mainpage.component.ts
+++ src/app/pages/mainpage/mainpage.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class MainpageComponent {
   botaoSelecionado: boolean = false;
+  armaSelecionada: boolean = false;
   imagemArma: string = '';
   corArma: string = '';
   animateImage: boolean = false;
@@ -52,8 +53,9 @@ export class MainpageComponent {
   }
 
   calibrumSelect() {
-    if (this.imagemArma != 'Calibrum') {
+    if (this.imagemArma != 'Calibrum' || document.getElementById("nomeArmaSelecionada")!.style.opacity == "0") {
       var audioArma = new Audio('assets/aud/Calibrum.ogg');
+      audioArma.volume = 0.3;
       audioArma.play();
       this.corArma = '#07ccc9fd'
       document.getElementById("imagemArma")!.style.opacity = "0";
@@ -65,14 +67,20 @@ export class MainpageComponent {
         document.getElementById("videoBackground")!.style.filter = "blur(4px)"
         this.imagemArma = 'Calibrum';
         document.getElementById("imagemArma")!.style.opacity = "1";
-      }, 600)
+      }, 400)
+    }
+    else {
+      document.getElementById("imagemArma")!.style.opacity = "0";
+      document.getElementById("nomeArmaSelecionada")!.style.opacity = "0";
+      document.getElementById("videoBackground")!.style.filter = "blur(0px)"
+      this.armaSelecionada = false;
     }
   }
 
   severumSelect() {
-    if (this.imagemArma != 'Severum') {
+    if (this.imagemArma != 'Severum' || document.getElementById("nomeArmaSelecionada")!.style.opacity == "0") {
       var audioArma = new Audio('assets/aud/Severum.ogg');
-      audioArma.volume = 0.2
+      audioArma.volume = 0.3;
       audioArma.play();
       this.corArma = '#cf0079'
       document.getElementById("imagemArma")!.style.opacity = "0";
@@ -84,13 +92,20 @@ export class MainpageComponent {
         this.imagemArma = 'Severum';
         document.getElementById("videoBackground")!.style.filter = "blur(4px)"
         document.getElementById("imagemArma")!.style.opacity = "1";
-      }, 600)
+      }, 400)
+    }
+    else {
+      document.getElementById("imagemArma")!.style.opacity = "0";
+      document.getElementById("nomeArmaSelecionada")!.style.opacity = "0";
+      document.getElementById("videoBackground")!.style.filter = "blur(0px)"
+      this.armaSelecionada = false;
     }
   }
 
   gravitumSelect() {
-    if (this.imagemArma != 'Gravitum') {
+    if (this.imagemArma != 'Gravitum' || document.getElementById("nomeArmaSelecionada")!.style.opacity == "0") {
       var audioArma = new Audio('assets/aud/Gravitum.ogg');
+      audioArma.volume = 0.3;
       audioArma.play();
       this.corArma = '#C331ED'
       document.getElementById("imagemArma")!.style.opacity = "0";
@@ -102,13 +117,20 @@ export class MainpageComponent {
         document.getElementById("videoBackground")!.style.filter = "blur(4px)"
         this.imagemArma = 'Gravitum';
         document.getElementById("imagemArma")!.style.opacity = "1";
-      }, 600)
+      }, 400)
+    }
+    else {
+      document.getElementById("imagemArma")!.style.opacity = "0";
+      document.getElementById("nomeArmaSelecionada")!.style.opacity = "0";
+      document.getElementById("videoBackground")!.style.filter = "blur(0px)"
+      this.armaSelecionada = false;
     }
   }
 
   infernumSelect() {
-    if (this.imagemArma != 'Infernum') {
+    if (this.imagemArma != 'Infernum' || document.getElementById("nomeArmaSelecionada")!.style.opacity == "0") {
       var audioArma = new Audio('assets/aud/Infernum.ogg');
+      audioArma.volume = 0.3;
       audioArma.play();
       this.corArma = '#3098F6'
       document.getElementById("imagemArma")!.style.opacity = "0"
@@ -120,26 +142,40 @@ export class MainpageComponent {
         document.getElementById("nomeArmaSelecionada")!.style.opacity = "1";
         this.imagemArma = 'Infernum';
         document.getElementById("imagemArma")!.style.opacity = "1";
-      }, 600)
+      }, 400)
+    }
+    else {
+      document.getElementById("imagemArma")!.style.opacity = "0";
+      document.getElementById("nomeArmaSelecionada")!.style.opacity = "0";
+      document.getElementById("videoBackground")!.style.filter = "blur(0px)"
+      this.armaSelecionada = false;
     }
   }
 
   crescendumSelect() {
-    if (this.imagemArma != 'Crescendum') {
+    if (this.imagemArma != 'Crescendum' || document.getElementById("nomeArmaSelecionada")!.style.opacity == "0") {
+      this.botaoSelecionado = true;
       var audioArma = new Audio('assets/aud/Crescendum.ogg');
+      audioArma.volume = 0.3;
       audioArma.play();
       this.corArma = '#e6e6e6'
       document.getElementById("imagemArma")!.style.opacity = "0";
       document.getElementById("nomeArmaSelecionada")!.style.opacity = "0";
       this.corArma
-      this.botaoSelecionado = true;
       document.getElementById("videoBackground")!.style.filter = "blur(4px)"
       setTimeout(() => {
         document.getElementById("nomeArmaSelecionada")!.style.color = "#e6e6e6";
         document.getElementById("nomeArmaSelecionada")!.style.opacity = "1";
         this.imagemArma = 'Crescendum';
         document.getElementById("imagemArma")!.style.opacity = "1";
-      }, 600)
+      }, 400)
+      this.armaSelecionada == true;
+    }
+    else {
+      document.getElementById("imagemArma")!.style.opacity = "0";
+      document.getElementById("nomeArmaSelecionada")!.style.opacity = "0";
+      document.getElementById("videoBackground")!.style.filter = "blur(0px)"
+      this.armaSelecionada = false;
     }
   }
 
